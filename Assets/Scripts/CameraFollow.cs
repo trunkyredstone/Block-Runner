3d9e843ddd251632e7c93e8500a3cca55ff619c5
trunkyredstone <romano@lucchesi.co.uk>
2018-05-05 18:33:41 +0100

    Added Mechanics
    Added the basic moving mechanic and the camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject target;
+	public Vector3 offset;
+
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, 0.0125f);
+		transform.LookAt (target.transform);
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;
+
+	void Update () {
+		transform.position += new Vector3 ((0-speed)*Time.deltaTime,0,0);
+	}
+}
